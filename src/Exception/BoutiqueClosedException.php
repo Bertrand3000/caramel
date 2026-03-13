07c7990d2e2a0ae48db44dd73ae2d5093d1e64a4
Bertrand Legendre <bertrand.legendre@assurance-maladie.fr>
2026-03-13 12:11:44 +0100

    Améliorations mineures des messages d'erreur.

--- src/Exception/BoutiqueClosedException.php
+++ src/Exception/BoutiqueClosedException.php
@@ -10,6 +10,6 @@ final class BoutiqueClosedException extends AccessDeniedHttpException
 {
     public function __construct()
     {
-        parent::__construct('La boutique n\'est pas encore ouverte, elle ouvrira au cours de la journée, merci pour votre patience.');
+        parent::__construct('La boutique est actuellement fermée. Nous vous remercions pour votre compréhension.');
     }
 }
